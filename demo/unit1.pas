8b791636c2b035aeae275b438938871c963476be
lcm <lcm@tmaid.com>
2020-11-30 11:50:25 +0100

    First version 0.1

---
+++ demo/unit1.pas
@@ -0,0 +1,120 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Variants, MediaInfoDll;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    OpenDialog1: TOpenDialog;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  HandleMI: Cardinal;
+  {$IFDEF WINDOWS}
+  To_Display: WideString;
+  CR: WideString;
+  filename: Widestring;
+  {$ELSE}
+  To_Display: String;
+  CR: String;
+  filename: String;
+  {$ENDIF}
+
+begin
+  CR:=Chr(13) + Chr(10);
+  {$IF  defined(WINDOWS)}
+  if (MediaInfoDLL_Load('mediainfo.dll')=false) then
+  begin
+      Memo1.Text := 'Error while loading mediainfo.dll';
+      exit;
+  end;
+  {$ELSEIF defined(DARWIN)}
+    if (MediaInfoDLL_Load('libmediainfo.0.dylib')=false) then
+  begin
+      Memo1.Text := 'Error while loading libmediainfo.0.dylib';
+      exit;
+  end;
+  {$ELSE }     // Linux
+  if (MediaInfoDLL_Load('libmediainfo.so.0')=false) then
+  begin
+      Memo1.Text := 'Error while loading libmediainfo.so.0';
+      exit;
+  end;
+  {$ENDIF}
+  To_Display := MediaInfo_Option (0, 'Info_Version', '') + CR;
+  if OpenDialog1.Execute then
+    begin
+      filename := OpenDialog1.Filename;
+
+      HandleMI := MediaInfo_New();
+      To_Display := To_Display + 'Open' + CR;
+      {$IFDEF WINDOWS}
+      To_Display := To_Display + format('%d', [MediaInfo_Open(HandleMI, PWideChar(filename))]);
+      {$ELSE}
+       To_Display := To_Display + format('%d', [MediaInfo_Open(HandleMI, PChar(filename))]);
+      {$ENDIF}
+
+      To_Display := To_Display + CR + CR + 'Inform with Complete=false' + CR;
+      MediaInfo_Option (HandleMI, 'Complete', '');
+      To_Display := To_Display + MediaInfo_Inform(HandleMI, 0);
+
+      To_Display := To_Display + CR + CR + 'Inform with Complete=true' + CR;
+      MediaInfo_Option (HandleMI, 'Complete', '1');
+      To_Display := To_Display + MediaInfo_Inform(HandleMI, 0);
+
+      To_Display := To_Display + CR + CR + 'Custom Inform' + CR;
+      MediaInfo_Option (HandleMI, 'Inform', 'General;Example : FileSize=%FileSize%');
+      To_Display := To_Display + MediaInfo_Inform(HandleMI, 0);
+      MediaInfo_Option (HandleMI, 'Inform', '');
+      //To_Display := To_Display + CR + CR + 'GetI with Stream=General and Parameter:=17' + CR;
+      //To_Display := To_Display + MediaInfo_GetI(HandleMI, Stream_General, 0, 17, Info_Text);
+      //
+      //To_Display := To_Display + CR + CR + 'Count_Get with StreamKind=Stream_Audio' + CR;
+      //To_Display := To_Display + format('%d', [MediaInfo_Count_Get(HandleMI, Stream_Audio, -1)]);
+      //
+      //To_Display := To_Display + CR + CR + 'Get with Stream:=General and Parameter=^AudioCount^' + CR;
+      //To_Display := To_Display + MediaInfo_Get(HandleMI, Stream_General, 0, 'AudioCount', Info_Text, Info_Name);
+      //
+      //To_Display := To_Display + CR + CR + 'Get with Stream:=Audio and Parameter=^StreamCount^' + CR;
+      //To_Display := To_Display + MediaInfo_Get(HandleMI, Stream_Audio, 0, 'StreamCount', Info_Text, Info_Name);
+      //
+      To_Display := To_Display + CR + CR + 'Get with Stream:=General and Parameter=^FileSize^' + CR;
+      To_Display := To_Display + MediaInfo_Get(HandleMI, Stream_General, 0, 'FileSize', Info_Text, Info_Name);
+
+      To_Display := To_Display + CR + CR + 'Close' + CR;
+      MediaInfo_Close(HandleMI);
+      MediaInfo_Delete(HandleMI);
+      Memo1.Text := To_Display;
+
+    end;
+
+
+end;
+
+end.
+
---
+++ src/MediaInfoDLL.pas
@@ -0,0 +1,163 @@
+unit MediaInfoDll;
+{$mode objfpc}{$H+}
+{
+  MediaInfoLib Interface for Lazarus  v 0.1
+  from MediaInfoLib (MediaInfo.dll v0.7.7.6) Interface for Delphi
+    (c)2008 by Norbert Mereg (Icebob)
+    http://MediaArea.net/MediaInfo
+}
+
+// Defines how the DLL is called
+// Converted to Lazarus (only for windows) Jurassic Pork November 2020
+// tested with MediaInfoLib v20.09
+// WINDOWS -> mediainfo.dll    tested on Windows 10 Lazarus 2.0.10 fpc 3.2
+// LINUX -> libmediainfo.so.0  tested on Centos 8.1 Lazarus 2.0.10 fpc 3.2
+// MAC Darwin -> libmediainfo.0.dylib     Not tested
+
+interface
+ uses   LCLIntf, LCLType;
+
+
+type TMIStreamKind =
+(
+    Stream_General,
+    Stream_Video,
+    Stream_Audio,
+    Stream_Text,
+    Stream_Other,
+    Stream_Image,
+    Stream_Menu,
+    Stream_Max
+);
+
+type TMIInfo =
+(
+    Info_Name,
+    Info_Text,
+    Info_Measure,
+    Info_Options,
+    Info_Name_Text,
+    Info_Measure_Text,
+    Info_Info,
+    Info_HowTo,
+    Info_Max
+);
+
+type TMIInfoOption =
+(
+    InfoOption_ShowInInform,
+    InfoOption_Reserved,
+    InfoOption_ShowInSupported,
+    InfoOption_TypeOfValue,
+    InfoOption_Max
+);
+
+type TMIFileOptions =
+(
+  FileOptions_Nothing,
+  FileOptions_Recursive,
+  FileOptions_CloseAll,
+  FileOptions_xxNonexx,
+  FileOptions_Max
+);
+
+
+var
+  LibHandle: THandle = 0;
+
+ {$IFDEF WINDOWS}
+  // Unicode methods
+  MediaInfo_New:        function  (): THandle stdcall;
+  MediaInfo_Delete:     procedure (Handle: THandle) stdcall;
+  MediaInfo_Open:       function  (Handle: THandle; File__: PWideChar): Cardinal  stdcall;
+  MediaInfo_Close:      procedure (Handle: THandle)  stdcall;
+  MediaInfo_Inform:     function  (Handle: THandle; Reserved: Integer): PWideChar  stdcall;
+  MediaInfo_GetI:       function  (Handle: THandle; StreamKind: TMIStreamKind; StreamNumber: Integer; Parameter: Integer;   KindOfInfo: TMIInfo): PWideChar  stdcall; //Default: KindOfInfo=Info_Text,
+  MediaInfo_Get:        function  (Handle: THandle; StreamKind: TMIStreamKind; StreamNumber: Integer; Parameter: PWideChar; KindOfInfo: TMIInfo; KindOfSearch: TMIInfo): PWideChar  stdcall; //Default: KindOfInfo=Info_Text, KindOfSearch=Info_Name
+  MediaInfo_Option:     function  (Handle: THandle; Option: PWideChar; Value: PWideChar): PWideChar stdcall;
+  MediaInfo_State_Get:  function  (Handle: THandle): Integer stdcall;
+  MediaInfo_Count_Get:  function  (Handle: THandle; StreamKind: TMIStreamKind; StreamNumber: Integer): Integer stdcall;
+  {$ELSE}
+  MediaInfo_New:       function  (): THandle  stdcall;
+  MediaInfo_Delete:    procedure (Handle: THandle) stdcall;
+  MediaInfo_Open:      function  (Handle: THandle; File__: PAnsiChar): Cardinal stdcall;
+  MediaInfo_Close:     procedure (Handle: THandle)  stdcall;
+  MediaInfo_Inform:    function  (Handle: THandle; Reserved: Integer): PAnsiChar  stdcall;
+  MediaInfo_GetI:      function  (Handle: THandle; StreamKind: TMIStreamKind; StreamNumber: Integer; Parameter: Integer; KindOfInfo: TMIInfo): PAnsiChar  stdcall; //Default: KindOfInfo=Info_Text
+  MediaInfo_Get:       function  (Handle: THandle; StreamKind: TMIStreamKind; StreamNumber: Integer; Parameter: PAnsiChar;   KindOfInfo: TMIInfo; KindOfSearch: TMIInfo): PAnsiChar  stdcall; //Default: KindOfInfo=Info_Text, KindOfSearch=Info_Name
+  MediaInfo_Option:    function  (Handle: THandle; Option: PAnsiChar; Value: PAnsiChar): PAnsiChar  stdcall;
+  MediaInfo_State_Get: function  (Handle: THandle): Integer stdcall;
+  MediaInfo_Count_Get: function  (Handle: THandle; StreamKind: TMIStreamKind; StreamNumber: Integer): Integer  stdcall;
+  {$ENDIF}
+
+  // Ansi methods
+  MediaInfoA_New:       function  (): THandle  stdcall;
+  MediaInfoA_Delete:    procedure (Handle: THandle) stdcall;
+  MediaInfoA_Open:      function  (Handle: THandle; File__: PAnsiChar): Cardinal stdcall;
+  MediaInfoA_Close:     procedure (Handle: THandle)  stdcall;
+  MediaInfoA_Inform:    function  (Handle: THandle; Reserved: Integer): PAnsiChar  stdcall;
+  MediaInfoA_GetI:      function  (Handle: THandle; StreamKind: TMIStreamKind; StreamNumber: Integer; Parameter: Integer; KindOfInfo: TMIInfo): PAnsiChar  stdcall; //Default: KindOfInfo=Info_Text
+  MediaInfoA_Get:       function  (Handle: THandle; StreamKind: TMIStreamKind; StreamNumber: Integer; Parameter: PAnsiChar;   KindOfInfo: TMIInfo; KindOfSearch: TMIInfo): PAnsiChar  stdcall; //Default: KindOfInfo=Info_Text, KindOfSearch=Info_Name
+  MediaInfoA_Option:    function  (Handle: THandle; Option: PAnsiChar; Value: PAnsiChar): PAnsiChar  stdcall;
+  MediaInfoA_State_Get: function  (Handle: THandle): Integer stdcall;
+  MediaInfoA_Count_Get: function  (Handle: THandle; StreamKind: TMIStreamKind; StreamNumber: Integer): Integer  stdcall;
+
+function MediaInfoDLL_Load(LibPath: string): boolean;
+
+implementation
+
+function MI_GetProcAddress(Name: PChar; var Addr: Pointer): boolean;
+begin
+  Addr := GetProcAddress(LibHandle, Name);
+  Result := Addr <> nil;
+end;
+
+function MediaInfoDLL_Load(LibPath: string): boolean;
+begin
+  Result := False;
+
+  if LibHandle = 0 then
+    LibHandle := LoadLibrary(PChar(LibPath));
+
+  if LibHandle <> 0 then
+  begin
+   {$IFDEF WINDOWS}
+    MI_GetProcAddress('MediaInfo_New',        Pointer(MediaInfo_New));
+    MI_GetProcAddress('MediaInfo_Delete',     Pointer(MediaInfo_Delete));
+    MI_GetProcAddress('MediaInfo_Open',       Pointer(MediaInfo_Open));
+    MI_GetProcAddress('MediaInfo_Close',      Pointer(MediaInfo_Close));
+    MI_GetProcAddress('MediaInfo_Inform',     Pointer(MediaInfo_Inform));
+    MI_GetProcAddress('MediaInfo_GetI',       Pointer(MediaInfo_GetI));
+    MI_GetProcAddress('MediaInfo_Get',        Pointer(MediaInfo_Get));
+    MI_GetProcAddress('MediaInfo_Option',     Pointer(MediaInfo_Option));
+    MI_GetProcAddress('MediaInfo_State_Get',  Pointer(MediaInfo_State_Get));
+    MI_GetProcAddress('MediaInfo_Count_Get',  Pointer(MediaInfo_Count_Get));
+    {$ELSE}       // LINUX   MAC DARWIN
+    MI_GetProcAddress('MediaInfoA_New',        Pointer(MediaInfo_New));
+    MI_GetProcAddress('MediaInfoA_Delete',     Pointer(MediaInfo_Delete));
+    MI_GetProcAddress('MediaInfoA_Open',       Pointer(MediaInfo_Open));
+    MI_GetProcAddress('MediaInfoA_Close',      Pointer(MediaInfo_Close));
+    MI_GetProcAddress('MediaInfoA_Inform',     Pointer(MediaInfo_Inform));
+    MI_GetProcAddress('MediaInfoA_GetI',       Pointer(MediaInfo_GetI));
+    MI_GetProcAddress('MediaInfoA_Get',        Pointer(MediaInfo_Get));
+    MI_GetProcAddress('MediaInfoA_Option',     Pointer(MediaInfo_Option));
+    MI_GetProcAddress('MediaInfoA_State_Get',  Pointer(MediaInfo_State_Get));
+    MI_GetProcAddress('MediaInfoA_Count_Get',  Pointer(MediaInfo_Count_Get));
+    {$ENDIF}
+    MI_GetProcAddress('MediaInfoA_New',       Pointer(MediaInfoA_New));
+    MI_GetProcAddress('MediaInfoA_Delete',    Pointer(MediaInfoA_Delete));
+    MI_GetProcAddress('MediaInfoA_Open',      Pointer(MediaInfoA_Open));
+    MI_GetProcAddress('MediaInfoA_Close',     Pointer(MediaInfoA_Close));
+    MI_GetProcAddress('MediaInfoA_Inform',    Pointer(MediaInfoA_Inform));
+    MI_GetProcAddress('MediaInfoA_GetI',      Pointer(MediaInfoA_GetI));
+    MI_GetProcAddress('MediaInfoA_Get',       Pointer(MediaInfoA_Get));
+    MI_GetProcAddress('MediaInfoA_Option',    Pointer(MediaInfoA_Option));
+    MI_GetProcAddress('MediaInfoA_State_Get', Pointer(MediaInfoA_State_Get));
+    MI_GetProcAddress('MediaInfoA_Count_Get', Pointer(MediaInfoA_Count_Get));
+
+    Result := True;
+  end;
+end;
+
+
+end.
